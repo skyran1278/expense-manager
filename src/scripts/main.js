1e73a7e6367f8cc18a26e64beedf1fe7d60b0f49
skyran1278 <skyran1278@gmail.com>
2017-05-07 14:17:54 +0800

    移動按鈕位置

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,6 +23,86 @@ const database = firebase.database();
 //   var email         = error.email;      // 使用者所使用的 Email
 //   var credential    = error.credential;
 // });
+(function () {
+    // body...
+    const str = `
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="./index.html">
+                    <img alt="Brand" src="./img/icon_48.png" onError="this.onerror=null;this.src='./img/icon700.svg;">
+                </a>
+                <a class="navbar-brand" href="./index.html">Expense Manager</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./read.html">Detailed</a></li>
+                    <li><a href="./create.html">Create</a></li>
+                    <!-- <li><a href="#">Sign in</a></li> -->
+                </ul>
+            </div>
+        </div>
+    `;
+    $('#nav').html(str);
+    // console.log(str);
+    // document.querySelector('#nav').innerHTML = str;
+}());
+
+function productForm() {
+    // body...
+    const str = `
+    <div class="form-group">
+        <label for="number">How much did you spend?</label>
+        <input type="number" class="form-control" name="number" id="number" placeholder="Amount of Consumption" required>
+    </div>
+    <div class="form-group">
+        <label for="type">Consumption Type</label>
+        <!-- <br>
+        <input class="btn btn-primary" name="type" id="type" type="button" value="Meal">
+        <input class="btn btn-info" name="type" id="type" type="button" value="Life">
+        <input class="btn btn-success" name="type" id="type" type="button" value="Entertainment">
+        <input class="btn btn-warning" name="type" id="type" type="button" value="Traffic">
+        <input class="btn btn-danger" name="type" id="type" type="button" value="Others"> -->
+        <!-- <br>
+        <div class="btn-group" role="group" aria-label="...">
+            <button type="button" class="btn btn-default ">Meal</button>
+            <button type="button" class="btn btn-default ">Life</button>
+            <button type="button" class="btn btn-default ">Entertainment</button>
+            <button type="button" class="btn btn-default ">Traffic</button>
+            <button type="button" class="btn btn-default ">Others</button>
+        </div> -->
+        <select class="form-control" name="type" id="type" required>
+            <option value="">Select</option>
+            <option value="Meal">Meal</option>
+            <option value="Life">Life</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Traffic">Traffic</option>
+            <option value="Others">Others</option>
+            <option value="Income">Income</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="date">When did you spend?</label>
+        <input type="date" class="form-control" name="date" id="date" placeholder="2016/10/10" required>
+    </div>
+    <div class="form-group">
+        <label for="title">What did you buy?</label>
+        <input type="text" class="form-control" name="title" id="title" placeholder="Please enter the name of the consumer, Ex. Lunch, dinner, watch the movie" required>
+    </div>
+    <button type="submit" class="btn btn-success">Submit</button>
+    <a href="./index.html">
+        <button type="button" class="btn btn-default">Home</button>
+    </a>
+    `;
+    $('#add-form').html(str);
+}
 
 function writeAccountData(id, title, type, number, date) {
     const accountRef = database.ref(`skyran/${id}`);
@@ -189,8 +269,8 @@ function loadChart(rawData) {
             // label: '',
             data: [Income, Expense],
             backgroundColor: [
-                'rgba(91, 192, 235, 1)',
-                'rgba(155, 197, 61,1)'
+                '#36A2EB',
+                '#FF6384'
             ]
             // borderColor: [
             //     'rgba(102, 153, 204, 1)',
@@ -252,33 +332,77 @@ function loadChart(rawData) {
             //     }
             // }
     };
+    // const axes = [{
+    //     gridLines: {
+    //         display: false
+    //     },
+    //     ticks: {
+    //         min: 0,
+    //         // max: 1000
+    //     }
+    // }];
+    const options = {
+        legend: {
+            display: false
+        },
+        scales: {
+            xAxes: [{
+                gridLines: {
+                    display: false
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    min: 0
+                    // max: 1000
+                }
+            }]
+        }
+    };
     // const myPieChart = new Chart(ctxRef, {
     const dataChart = new Chart(ctxDataChart, {
         // Chart(ctxRef, {
         data,
         type: 'bar',
-        options: {
-            legend: {
-                display: false
-            }
-        }
+        options
     });
+    // console.log("hey");
+}
+
+function readChart() {
+    // body...
+    const accountRef = database.ref('skyran/');
+    const infoRef = document.querySelector('#data-chart-info');
+    // const dataTableRef = document.querySelector('#data-table');
+
+    accountRef.once('value').then((snapshot) => {
+        const data = snapshot.val();
+        // console.log(data);
+        if (data === null) {
+            // str += '<h4>目前沒有資料喔！</h4>';
+            // dataTableRef.innerHTML = '<h4>Creat New Expense</h4>';
+            infoRef.innerHTML = '<h4>Have no data</h4>';
+        } else {
+            loadChart(data);
+            }
+        });
+    console.log("readChart");
 }
 
 function readAccountData() {
     let str = `
-    <thead>
-      <tr>
-        <th class="col-md-1"></th>
-        <th class="col-md-2">Title</th>
-        <th class="col-md-2">Type</th>
-        <th class="col-md-2">Number</th>
-        <th class="col-md-2">Time</th>
-        <th class="col-md-2">Edit</th>
-        <th class="col-md-1"></th>
-      </tr>
-    </thead>
-  `;
+        <thead>
+            <tr>
+                <th class="col-md-1"></th>
+                <th class="col-md-2">Title</th>
+                <th class="col-md-2">Type</th>
+                <th class="col-md-2">Number</th>
+                <th class="col-md-2">Time</th>
+                <th class="col-md-2">Edit</th>
+                <th class="col-md-1"></th>
+            </tr>
+        </thead>
+    `;
     const accountRef = database.ref('skyran/');
     const infoRef = document.querySelector('#data-chart-info');
     const dataTableRef = document.querySelector('#data-table');
@@ -292,23 +416,63 @@ function readAccountData() {
             infoRef.innerHTML = '<h4>Have no data</h4>';
         } else {
             loadChart(data);
+
+            // console.log("hi");
+            const arrs = [];
+            let i = 0;
             Object.keys(data).forEach((key) => {
+                arrs[i] = [];
+                arrs[i].push(data[key].date);
+                arrs[i].push(data[key].title);
+                arrs[i].push(data[key].type);
+                arrs[i].push(data[key].number);
+                arrs[i].push(key);
+                i += 1;
+
+
+                // console.log(data[key]);
+            //     // str +=
+            //     //     `
+            //     // <tr>
+            //     //     <td></td>
+            //     //     <td>${data[key].title}</td>
+            //     //     <td>${data[key].type}</td>
+            //     //     <td>$ ${data[key].number}</td>
+            //     //     <td>${data[key].date}</td>
+            //     //     <td>
+            //     //         <button type="button" class="btn btn-primary update-btn" data-id="${key}">Update</button>
+            //     //         <button type="button" class="btn btn-danger delete-btn" data-id="${key}">Delete</button>
+            //     //     </td>
+            //     //     <td></td>
+            //     // </tr>
+            //     // `;
+            });
+
+            arrs.sort((a, b) => {
+                if (a < b) return 1;
+                if (a > b) return -1;
+                return 0;
+            });
+
+            arrs.forEach((key) => {
+                // console.log(key);
                 str +=
                     `
-          <tr>
-            <td></td>
-            <td>${data[key].title}</td>
-            <td>${data[key].type}</td>
-            <td>$ ${data[key].number}</td>
-            <td>${data[key].date}</td>
-            <td>
-              <button type="button" class="btn btn-primary update-btn" data-id="${key}">Update</button>
-              <button type="button" class="btn btn-danger delete-btn" data-id="${key}">Delete</button>
-            </td>
-            <td></td>
-          </tr>
-        `;
+                <tr>
+                    <td></td>
+                    <td>${key[1]}</td>
+                    <td>${key[2]}</td>
+                    <td>$ ${key[3]}</td>
+                    <td>${key[0]}</td>
+                    <td>
+                        <button type="button" class="btn btn-primary update-btn" data-id="${key[4]}">Update</button>
+                        <button type="button" class="btn btn-danger delete-btn" data-id="${key[4]}">Delete</button>
+                    </td>
+                    <td></td>
+                </tr>
+                `;
             });
+            // console.log(arrs);
             document.querySelector('#data-table').innerHTML = str;
             updateBtnListener();
             deleteBtnListener();
@@ -336,7 +500,7 @@ function updateData(id, title, type, number, date) {
     });
     accountRef.on('value', () => {
         // console.log('success');
-        window.location = './index.html';
+        window.location = './read.html';
     });
 }
 
@@ -360,17 +524,22 @@ function submitListener(submitType) {
 }
 
 const path = window.location.pathname;
-
+console.log(path);
 switch (path) {
     case '/create.html':
+        productForm();
         submitListener('create');
         break;
     case '/update.html':
+        productForm();
         readFormData();
         submitListener('update');
         break;
-    default:
+    case '/read.html':
         readAccountData();
+        break;
+    default:
+        readChart();
 }
 
 
