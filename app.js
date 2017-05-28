1baae301bbf4f5812a672fa8616d62c25a0dada3
skyran1278 <skyran1278@gmail.com>
2017-05-28 21:48:05 +0800

    favicon.ico

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 // app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /docs
-app.use(favicon(path.join(__dirname, 'docs', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'docs/img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
